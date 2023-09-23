f0b1d18b31ffcf731828082c48a8e53f65da3079
shariqfarooq123 <shariqfarooq00@gmail.com>
2023-09-23 16:41:23 +0300

    posts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,31 @@
 import streamlit as st
-
-st.header('Hello 🌎!')
-if st.button('Balloons?'):
-    st.balloons()
-
-
-import streamlit as st
 from google.cloud import firestore
 
-# Authenticate to Firestore with the JSON account key.
+# With:
 import json
 key_dict = json.loads(st.secrets["textkey"])
 creds = firestore.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="streamlit-reddit")
 
-# Create a reference to the Google post.
-doc_ref = db.collection("posts").document("Google")
+# Streamlit widgets to let a user create a new post
+title = st.text_input("Post title")
+url = st.text_input("Post url")
+submit = st.button("Submit new post")
 
-# Then get the data at that reference.
-doc = doc_ref.get()
+# Once the user has submitted, upload it to the database
+if title and url and submit:
+	doc_ref = db.collection("posts").document(title)
+	doc_ref.set({
+		"title": title,
+		"url": url
+	})
 
-# Let's see what we got!
-st.write("The id is: ", doc.id)
-st.write("The contents are: ", doc.to_dict())
+# And then render each post, using some light Markdown
+posts_ref = db.collection("posts")
+for doc in posts_ref.stream():
+	post = doc.to_dict()
+	title = post["title"]
+	url = post["url"]
+
+	st.subheader(f"Post: {title}")
+	st.write(f":link: [{url}]({url})")
