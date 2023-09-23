184a7920ef493107c684a1a4b6f76f9e89d1ecf3
shariqfarooq123 <shariqfarooq00@gmail.com>
2023-09-23 16:53:07 +0300

    final working

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from google.oauth2 import service_account
 import json
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.Client(credentials=creds, project="streamlit-reddit")
+db = firestore.Client(credentials=creds)
 
 # Streamlit widgets to let a user create a new post
 title = st.text_input("Post title")
