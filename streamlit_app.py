07ae20eb4be40b0c4f73b97bc2b0cca978d1b996
shariqfarooq123 <shariqfarooq00@gmail.com>
2023-09-27 06:03:46 +0300

    add usy data; firestore queues; user prefs; collections

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,27 +4,22 @@ from google.oauth2 import service_account
 import datasets
 import json
 import uuid
-from dataclasses import dataclass
+import random
 
 st.set_page_config(layout="wide")
 
-# ALL_METHODS = ["ours", "gligen", "layoutgpt", "llmgrounded", "stablediffusion"]
-ALL_METHODS = ['image', 'depth']
+ALL_METHODS = ["ours", "gligen", "layoutgpt", "llmgrounded", "stablediffusion"]
 caption_col = "caption"
+CURRENT_2AFC_KEY = "curr_2afc"
+USER_PREFFERENCES_COLLECTION = "userpreferences"
 
 
-class TwoAFC:
-	def __init__(self, data):
-		self.data = data
-		inds = list(range(len(data)))
 
 
 def load_data():
 	with st.spinner("Loading ..."):
-		# data = datasets.load_dataset("shariqfarooq/cs323_densepred_depth", streaming=True, split='test', keep_in_memory=True)
-		data = datasets.load_dataset("shariqfarooq/cs323_densepred_depth", split='test', keep_in_memory=True)
-		# data = data.shuffle(buffer_size=10)
-	return iter(data)
+		data = datasets.load_dataset('shariqfarooq/USYllmblue', split='train')
+	return data
 
 def get_state(*args):
 	out = [getattr(st.session_state, arg) for arg in args]
@@ -48,63 +43,117 @@ def clear_checkboxes():
 		if key.startswith("checkbox_"):
 			st.session_state[key] = False
 
-def submit_button_label():
-	if any(st.session_state[f"checkbox_{method}"] for method in ALL_METHODS):
+def submit_button_label(methods):
+	if any(st.session_state[f"checkbox_{method}"] for method in methods):
 		return "Submit"
 	else:
 		return "I'm not sure"
 
-
-def preference_ui(idd, prompt, method2image):
+def preference_ui(twoafc_dict):
+	twoafc_data = load_2afc_data(twoafc_dict)
 	st.title("Select your preference")
-	# with st.form("my_form"):
-	st.write("Which image do you prefer?")
-	cols = st.columns(len(method2image))
+	st.write()
+	st.info(twoafc_data['caption'])
+	cols = st.columns(2)
+	method1_image = twoafc_data['method1']
+	method2_image = twoafc_data['method2']
+	method1_name = twoafc_data['method1_name']
+	method2_name = twoafc_data['method2_name']
+
+	method2image = {method1_name: method1_image, method2_name: method2_image}
 	for col, (method, image) in zip(cols, method2image.items()):
 		with col:
 			st.checkbox("", key=f"checkbox_{method}", on_change=make_checkboxes_exclusive, kwargs=dict(selected_key=f"checkbox_{method}"))
-			st.image(image, caption=method, width=300)
+			st.image(image)
 
-	st.button(submit_button_label(), on_click=on_submit, kwargs=dict(idd=idd))
+	st.button(submit_button_label(method2image.keys()), on_click=on_submit, kwargs=dict(twoafc_dict=twoafc_dict))
 
 
 def update_data():
-	with st.spinner("Fetching row ..."):
-		row = next(get_state("data"))
-		set_state(row=row)
+	with st.spinner("Fetching ..."):
+		curr_twoafc = get_next_2afc()
+		set_state(**{CURRENT_2AFC_KEY: curr_twoafc})
 
-def on_submit(idd):
-	update_data()
+
+def on_submit(twoafc_dict):
 	# upload 
-	for method in method2image.keys():
+	methods = [twoafc_dict['method1'], twoafc_dict['method2']]
+	for method in methods:
 		if st.session_state[f"checkbox_{method}"]:
-			upload_preference(idd, method)
+			ref = twoafc_dict.pop('ref')
+			upload_preference(twoafc_dict, method)
+			# delete the element from the queue
+			ref.reference.delete()
 	clear_checkboxes()
+	update_data()
 
 def get_current_set():
-	if get_state("row") is None:
+	if not hasattr(st.session_state, CURRENT_2AFC_KEY) or get_state(CURRENT_2AFC_KEY) is None:
 		update_data()
-	
-	row = get_state("row")
-	st.write(row)
-	# idd = row["id"]
-	idd = str(uuid.uuid4())
-	method2image = {method: row[method] for method in ALL_METHODS}
-	return idd, method2image
+	return get_state(CURRENT_2AFC_KEY)
 
-def upload_preference(idd, method):
+def upload_preference(twoafc_dict, chosen_method):
 	db : firestore.Client
 	db, user_id = get_state("db", "user_id")
-	db.collection("preferences").add({
-		"user_id": user_id,
-		"method": method,
-		"entry_id": idd
-	})
+	col = db.collection(USER_PREFFERENCES_COLLECTION)
+	col.add({'user_id': user_id, 'chosen': chosen_method, **twoafc_dict})
+
+def get_next_2afc():
+	db : firestore.Client
+	db, user_id = get_state("db", "user_id")
+	# get number of max queues to check
+	max_queues = db.collection("study").document("max_queues").get().to_dict()["value"]
+	# get the non-empty queue
+	queue_no = 0
+	while True:
+		queue_name = f"2afc-{queue_no}"
+		queue_ref = db.collection(f"study/queues/{queue_name}")
+		# check if queue is empty
+		if len(queue_ref.limit(1).get()) > 0:
+			break
+		queue_no += 1
+
+		if queue_no >= max_queues:
+			return None
+	
+	# debug with queue debug queue
+	# queue_name = f"2afc-debug"
+	queue_ref = db.collection(f"study/queues/{queue_name}")
+	
+	# get the first element of the queue
+	# we are using firestore as a queue
+	# twoafc_doc = next(queue_ref.limit(1).stream())
+
+	# get a random element of the queue
+	choices = queue_ref.limit(10).get()
+	if len(choices) == 0:
+		return None
+	twoafc_doc = random.choice(choices)
+	twoafc_dict = twoafc_doc.to_dict()
+	twoafc_dict['ref'] = twoafc_doc
+	# delete the element from the queue
+	# twoafc_doc.delete()
+	# return the element
+	return twoafc_dict
+
+def load_2afc_data(twoafc_dict):
+	data = get_state("data")
+	row = data[twoafc_dict["idx"]]
+	return dict(
+		id=twoafc_dict["id"],
+		method1=row[twoafc_dict["method1"]],
+		method2=row[twoafc_dict["method2"]],
+		method1_name=twoafc_dict["method1"],
+		method2_name=twoafc_dict["method2"],
+		caption=row[caption_col]
+	)
+
 	
 
 def display_preferences():
+	# debug only
 	db, user_id = get_state("db", "user_id")
-	preferences = db.collection("preferences").where("user_id", "==", user_id).stream()
+	preferences = db.collection(USER_PREFFERENCES_COLLECTION).where("user_id", "==", user_id).stream()
 	# preferences = db.collection("preferences").stream()
 	for pref in preferences:
 		st.write(pref.to_dict())
@@ -117,9 +166,12 @@ if not hasattr(st.session_state, "user_id"):
 	set_state(db=db, data=load_data(), user_id=str(uuid.uuid4()), row=None)
 
 
-st.write(f"Hello world {get_state('user_id')}")
-idd, method2image = get_current_set()
-st.write(idd)
-preference_ui(idd, "Which image do you prefer?", method2image)
-display_preferences()
-st.write(st.session_state)
+st.title("Welcome! Thank you for participating in our study.")
+"""
+## Instructions
+#### Select the image (by clicking the checkbox above it) that most accurately represents the provided textual description (in blue). 
+Image quality is not a factor to consider. Please thoroughly examine the image before making your selection. Your task is to identify the image that, in your judgment, aligns most closely with the provided description. It may be possible that few objects in the descriptions may not be present in any of the images. In that case choose the image that according to you has the maximum information encompassing the text. 
+Click 'Submit' to submit your response. If you are unsure, click 'I'm not sure'.
+"""
+twoafc_dict = get_current_set()
+preference_ui(twoafc_dict)
